80777c40d5a1059139d2b1e8fb9b993f7d6c14c0
pengshilan <978952122@qq.com>
2022-01-12 16:36:09 +0800

    trade

--- src/views/trade/TradeOperation/index.tsx
+++ src/views/trade/TradeOperation/index.tsx
@@ -1,6 +1,28 @@
-import { Box } from '@material-ui/core'
-import { FC } from 'react'
+import { Box, Theme } from "@material-ui/core";
+import { makeStyles, styled, withStyles, WithStyles } from "@material-ui/styles"
+import { FC } from "react";
+import Image from 'next/image'
 
+const useStyles = makeStyles<Theme>((theme) => ({
+  
+	
+}))
 export const TradeOperation: FC = () => {
-  return <Box width={324}>tradeoperation</Box>
+  const classes = useStyles()
+	return (
+    <Box py="12px" width={324} height={47} borderBottom='1px solid #2d2d3d'>
+      <Box display="flex" alignItems="center" justifyContent="space-between" px="20px">
+        <Box display="flex" alignItems="center">
+          <Box mr={1} width={24} height={24}>
+            <Image width={24} height={24} src="/images/btc.svg" alt=""></Image>
+          </Box>
+          <span>ETH-USDT</span>
+        </Box>
+        <div>
+          <span>交易对</span>
+        </div>
+      </Box>
+    </Box>
+  )
+  
 }
