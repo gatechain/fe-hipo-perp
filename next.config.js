8bb312a002dc7aac7b5f42575eef3e6300a3d8b1
Thone Liu <thorne_lew@126.com>
2022-01-28 15:04:25 +0800

    modify basePath perp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/prep' : '/',
+  basePath: process.env.NODE_ENV === 'production' ? '/perp' : '/',
   async redirects() {
     return [
       {
